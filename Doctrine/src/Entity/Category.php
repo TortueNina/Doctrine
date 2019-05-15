c1b6e301be3bfd2f78d6cf6d28fa648eaa355d6d
Vanina Diot <vanina.diot@gmail.com>
2019-05-15 14:41:52 +0200

    Doctrine Entity et Base de données

---
+++ Doctrine/src/Entity/Category.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
+ */
+class Category
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Article;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getArticle(): ?string
+    {
+        return $this->Article;
+    }
+
+    public function setArticle(string $Article): self
+    {
+        $this->Article = $Article;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
