3cda8550fab690e6d29b33d37777135daf437df9
Vanina Diot <vanina.diot@gmail.com>
2019-05-15 16:27:02 +0200

    Ajout title à Article

--- Doctrine/src/Entity/Article.php
+++ Doctrine/src/Entity/Article.php
@@ -21,6 +21,11 @@ class Article
      */
     private $content;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -34,6 +39,18 @@ class Article
     public function setContent(string $content): self
     {
         $this->content = $content;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
 
         return $this;
     }
